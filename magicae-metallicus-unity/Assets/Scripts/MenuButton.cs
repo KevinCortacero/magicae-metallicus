651a686a0ace229c615f3c62ed8881064a0df154
SebArola <sebastien.arola@laposte.net>
2018-11-05 16:36:06 +0100

    menu

---
+++ magicae-metallicus-unity/Assets/Scripts/MenuButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour {
+
+    public void START_GAME()
+    {
+        Application.LoadLevel("arena");
+    }
+
+    public void QUIT_GAME()
+    {
+        Application.Quit();
+    }
+}
