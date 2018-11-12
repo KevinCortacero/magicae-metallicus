c1366d1a28044660d7aa8d58f133bfaf2d146051
THOMAS-PC\Thomas <sauvage.thomas@gmail.com>
2018-11-12 14:20:01 +0100

    bugfix

--- magicae-metallicus-unity/Assets/Scripts/Model/Projectiles/ProjectileHolder.cs
+++ magicae-metallicus-unity/Assets/Scripts/Model/Projectiles/ProjectileHolder.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class ProjectileHolder {
 
+    [SerializeField]
     public GameObject projectile;
+    [SerializeField]
     public float remaining;
 
     public ProjectileHolder(GameObject projectile, float remaining) {
