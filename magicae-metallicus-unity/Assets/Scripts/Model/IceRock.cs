506f6b9095b426008a85a6ba39dd72878138fe00
THOMAS-PC\Thomas <sauvage.thomas@gmail.com>
2018-11-12 14:47:15 +0100

    ice melting

---
+++ magicae-metallicus-unity/Assets/Scripts/Model/IceRock.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceRock : RockScript {
+
+    private double start;
+    private float maxTime = 25f;
+
+    // Use this for initialization
+    void Start() {
+        this.start = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (this.pv <= 0) {
+            Destroy(gameObject);
+        }
+        double elapsed = Time.time - this.start;
+        float ratio = 1f - (float)(elapsed / this.maxTime);
+
+        Debug.Log("elapsed : " + elapsed + " ratio = " + ratio + " my scale = " + GetComponent<Transform>().localScale.x + "= " + GetComponent<Transform>().localScale.x * ratio);
+
+        GetComponent<Transform>().localScale = new Vector3(ratio, ratio, 0);
+        Debug.Log("scale = " + new Vector3(ratio, ratio, 0));
+        if (elapsed > 20) {
+            Debug.Log("destroy");
+            Destroy(gameObject);
+        }
+    }
+}
