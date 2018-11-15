e69632f13c296ac8ef7329fde21dc9f6a575a842
MariusLevin <pecchioli.mathieu@gmail.com>
2018-11-15 22:19:21 +0100

    Change rock generation & add loots

--- magicae-metallicus-unity/Assets/Scripts/RockScript.cs
+++ magicae-metallicus-unity/Assets/Scripts/RockScript.cs
@@ -5,21 +5,69 @@ using UnityEngine;
 public class RockScript : MonoBehaviour {
 
     public float pv;
+	private Vector3 start;
+	private Vector3 end;
+	private int direction;
+	private float speed;
+	private int nbTurnOfBuffer;
+
 	// Use this for initialization
 	void Start () {
-       
-       
+       // Init rock positions
+		start = transform.position;
+		end = transform.position;
+
+		speed = 1f;
+		nbTurnOfBuffer = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (this.pv <= 0)
-        {
-            Destroy(gameObject);
-        }
+		this.start = transform.position;
+		if (direction == 1) {
+			if (start[0] < end[0]) {
+				transform.position += new Vector3 (speed * Time.deltaTime, 0.0f, 0.0f);
+			}
+		}
+		else {
+			if (start[0] > end[0]) {
+				transform.position -= new Vector3 (speed * Time.deltaTime, 0.0f, 0.0f);
+			}
+		}
 	}
 
-	public int getNbObj () {
-		return 1;
+	public void setParams(int direction) {
+		this.direction = direction;
+	}
+
+	public Vector3 getLocation() {
+		return transform.position;
+	}
+
+	public void move(Vector3 d, int direction) {
+		this.direction = direction;
+		this.end += d;
+	}
+
+	public void moveDirectly(Vector3 d) {
+		transform.position += d;
+		start = transform.position;
+		end = transform.position;
+	}
+
+	public float getPv() {
+		return this.pv;
+	}
+
+	public void resetPv() {
+		this.pv = 3;
+	}
+
+	public void increaseBufferNumberOfTurn() {
+		this.nbTurnOfBuffer += 1;
+	}
+
+	public int getBufferNumberOfTurn() {
+		return this.nbTurnOfBuffer;
 	}
 }
